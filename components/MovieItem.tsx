cc8338d79fa63791445ba5ed2eff3612dd4c6200
pountzas <nikos@pountzas.gr>
2025-09-15 21:29:01 +0300

    Refactor MovieItem component styling for improved visual consistency

--- components/MovieItem.tsx
+++ components/MovieItem.tsx
@@ -24,16 +24,16 @@ const MovieItem = ({ movie }: { movie: Movie }) => {
             method: "POST"
           }}
         >
-          <View className="rounded bg-black/30">
+          <View className="rounded-t-2xl bg-black/30">
             <View className="px-3 pt-3">
-              <Text className="text-sm text-gray-800 truncate dark:text-gray-200">
+              <Text className="text-sm text-gray-200 truncate">
                 {movie.title}
               </Text>
               <View className="flex flex-row justify-between items-center">
                 <Text className="text-sm text-gray-300">
                   {movie.release_date.slice(0, 4)}
                 </Text>
-                <Text className="text-sm text-yellow-700 dark:text-yellow-500">
+                <Text className="text-sm text-yellow-500">
                   {movie.vote_average.toString().slice(0, 3)}/10
                 </Text>
               </View>
